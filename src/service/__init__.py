3964aea849bd6f25f055a08baee7a75dafdc1740
myzozoz <markusp_holopainen@yahoo.com>
2021-10-26 13:42:04 +0300

    Added clearer example return value

--- src/service/__init__.py
+++ src/service/__init__.py
@@ -1,11 +1,21 @@
 import api
 
 # input: list of tags
-# output: analyzed data
+# output: list of words with values in dict format, e.g.
+# {
+#   'word': 'exampleword',
+#   'value': 22
+# }
 def analyze_reviews(tags):
   games = api.fetch_games_with_tags(tags)
   reviews = api.fetch_reviews_for_games(games)
   
   # Code to analyze the reviews goes here
 
-  return reviews
+  return  [{
+   'word': 'exampleword',
+   'value': 22
+  }, {
+   'word': 'seconword',
+   'value': 11
+  }]
